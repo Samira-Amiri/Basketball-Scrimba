cddb1da915ebb2bdb07608d9d1fe6559624adf8a
samira.amiri130@gmail.com <samira.amiri130@gmail.com>
2024-02-05 04:27:30 -0800

    add Design

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-document.getElementById("score_home_el")
-document.getElementById("score_guest_el")
-document.getElementById("firstle")
-document.getElementById("secondle")
-document.getElementById("thirdle")
+let counthome = 0;
+let countguest = 0;
 
-let counthome =0  
-let countguest = 0
+function first() {
+    counthome += 1;
+    document.getElementById("score_home_el").textContent = counthome;
+}
 
-function first(){
-    counthome += 1
-    score_home_el.textContent = counthome
+function second() {
+    counthome += 2;
+    document.getElementById("score_home_el").textContent = counthome;
 }
-function second(){
-    counthome += 2 
-    score_home_el.textContent = counthome
-}
-function third(){
-    counthome += 3
-    score_home_el.textContent = counthome
 
+function third() {
+    counthome += 3;
+    document.getElementById("score_home_el").textContent = counthome;
 }
-function first(){
-    countguest += 1
-    score_guest_el.textContent = countguest
-}
-function second(){
-    countguest += 2 
-    score_guest_el.textContent = countguest
-}
-function third(){
-    countguest += 3
-    score_guest_el.textContent = countguest
 
+function firstguest() {
+    countguest += 1;
+    document.getElementById("score_guest_el").textContent = countguest;
+}
+
+function secondguest() {
+    countguest += 2;
+    document.getElementById("score_guest_el").textContent = countguest;
+}
+
+function thirdguest() {
+    countguest += 3;
+    document.getElementById("score_guest_el").textContent = countguest;
 }
